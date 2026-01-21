ec595a5340597c9058e6686da4b56b3122cc2b49
Karam-Aldabain <karamdabaeen41@gmail.com>
2026-01-21 20:46:54 +0300

    Allow php-fpm connections from nginx

--- edumentor-backend/start.sh
+++ edumentor-backend/start.sh
@@ -14,8 +14,8 @@ envsubst '$PORT $CORS_ORIGIN' < /etc/nginx/conf.d/default.conf.template > /etc/n
 echo "Using PORT=$PORT"
 nginx -t
 
-sed -i 's|^listen = .*|listen = 127.0.0.1:9000|' /usr/local/etc/php-fpm.d/www.conf
-sed -i 's|^;listen.allowed_clients = .*|listen.allowed_clients = 127.0.0.1|' /usr/local/etc/php-fpm.d/www.conf
+sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /usr/local/etc/php-fpm.d/www.conf
+sed -i 's|^;listen.allowed_clients = .*|;listen.allowed_clients = 127.0.0.1|' /usr/local/etc/php-fpm.d/www.conf
 
 php-fpm -D
 nginx -g 'daemon off;'
