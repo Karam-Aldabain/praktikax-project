28cbe9c9d4e7f12b74bff053b09a0326445649b2
Karam-Aldabain <karamdabaeen41@gmail.com>
2026-01-21 20:39:07 +0300

    Force php-fpm to listen on 127.0.0.1:9000

--- edumentor-backend/start.sh
+++ edumentor-backend/start.sh
@@ -10,5 +10,8 @@ envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/d
 echo "Using PORT=$PORT"
 nginx -t
 
+sed -i 's|^listen = .*|listen = 127.0.0.1:9000|' /usr/local/etc/php-fpm.d/www.conf
+sed -i 's|^;listen.allowed_clients = .*|listen.allowed_clients = 127.0.0.1|' /usr/local/etc/php-fpm.d/www.conf
+
 php-fpm -D
 nginx -g 'daemon off;'
