f133a364e7ed4c8b78a842645eaa56e9622a79e5
Karam-Aldabain <karamdabaeen41@gmail.com>
2026-01-21 20:28:13 +0300

    Bind nginx to Railway PORT

--- edumentor-backend/start.sh
+++ edumentor-backend/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ -z "$PORT" ]; then
+  PORT=8080
+fi
+
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
 php-fpm -D
 nginx -g 'daemon off;'
